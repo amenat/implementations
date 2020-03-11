f09548ebcba91dd349bc9d9e09e542180dd6df87
Ankush Menat <ankushmenat@gmail.com>
2020-03-11 19:29:41 +0530

    Picks random operation instead of multiplication

--- viswanath-constant/viswanath-const.py
+++ viswanath-constant/viswanath-const.py
@@ -2,27 +2,36 @@
 # Read more: https://en.wikipedia.org/wiki/Random_Fibonacci_sequence
 
 from random import choice
+from operator import add, sub
 import gmpy2
-gmpy2.get_context().precision=256
+gmpy2.get_context().precision=64
+
 
 def exp_rand_fib_n(n: int, simcount: int) -> int:
     '''
-    Returns expected value for nth term in random fibonacci sequence.
+    Returns list containing simulations of nth number in random fibonacci sequence.
     '''
+    n = n-1
+    ops = [add, sub]
     simulation = []
     for i in range(simcount):
-        f_0, f_1 = 1, 1
-        for j in range(n-2):
-            tmp = f_0 + choice([-1,1]) * f_1
+        f_0, f_1 = 0, 1
+        for j in range(n):
+            op = choice(ops)
+            tmp = op(f_0, f_1)
             f_0 = f_1
             f_1 = tmp
-        simulation.append(f_1)
+        simulation.append(abs(f_1))
     
-    return abs(sum(simulation) // simcount)
+    return simulation
 
 
+if __name__ == "__main__":
+    n = 200000
+    simcount = 100
+    fibn = exp_rand_fib_n(n, simcount)
+    nthroot = lambda x: gmpy2.root(x, n)
+    results = list(map(nthroot, fibn))
+    print(sum(results) / len(results))
 
-n = 100000
-fibn = exp_rand_fib_n(n, 100)
-
-print(gmpy2.root(fibn, n))
+    # returned: 1.13199499293206857608
